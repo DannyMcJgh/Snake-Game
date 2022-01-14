0c8567a5f402d5cea510e68d9be1f81bec2d1645
Danny <Danny@DESKTOP-J0GPR45>
2022-01-13 20:06:21 -0700

    Finished the basics of the game.

--- Snake Game/Snake.cpp
+++ Snake Game/Snake.cpp
@@ -129,6 +129,11 @@ void Logic()
 	}
 	if (x > width || x < 0 || y > height || y < 0)
 		gameOver = true;
+
+	for (int i = 0; i < nTail; i++)
+		if (tailX[i] == x && tailY[i] == y)
+			gameOver = true;
+
 	if (x == fruitX && y == fruitY)
 	{
 		
